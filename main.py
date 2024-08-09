22d8e903be43a7487529e5d27a7f41548a2600bb
DickyOsmondSieliewangi <138377290+DickyOsmondSieliewangi@users.noreply.github.com>
2024-08-09 22:01:26 +0700

    chore: Add initial code files and dependencies

---
+++ main.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+import pandas as pd
+import pykakasi
+from PIL import Image
+from paddleocr import PaddleOCR, draw_ocr, PPStructure, draw_structure_result, save_structure_res
+from googletrans import Translator
+from sudachipy import tokenizer,dictionary
+
+ocr = PaddleOCR(use_angle_cls=True, 
+                lang='japan', 
+                use_space_char = True, 
+                cls = True
+) # need to run only once to download and load model into memory
+
+#IMAGE_PATH = "./data/b.jpg"
+IMAGE_PATH = "./data/w.jpg"
+
+def process_image(image_path):
+    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+
+    #image preprocessing
+
+    resized = cv2.resize(img, (0,0), fx=4, fy=4)
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    thresh,im_bw = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    result = ocr.ocr(im_bw, cls=True)
+
+    boxes = [line[0] for line in result[0]]
+    txts = [line[1][0] for line in result[0]]
+    scores = [line[1][1] for line in result[0]]
+
+    #translate a line
+    translator = Translator()
+    translated = [translator.translate(txt, dest='en').text for txt in txts]
+
+    #tokenize each word
+
+    tokenizer_obj = dictionary.Dictionary(dict_type='full').create()
+    translator = Translator()
+    mode = tokenizer.Tokenizer.SplitMode.B
+    kks = pykakasi.kakasi()
+
+    #version 1
+    tokens = [[m.surface() for m in tokenizer_obj.tokenize(txt, mode)] for txt in txts]
+    translated_token = [[translator.translate(word, dest='en').text for word in token] for token in tokens]
+    romaji = [[''.join([item['hepburn'] for item in kks.convert(word)]) for word in token] for token in tokens]
+
+    result = list(zip(boxes, txts, translated, tokens, romaji, translated_token))
+    df = pd.DataFrame(result, columns=['Box','Japanese','Translated','Tokens','Romaji','Translated_Tokens'])
+    
+    return resized,df
+
+
+#example function used
+image, df = process_image(IMAGE_PATH)
+
+print(df)
+
+
+
+
+
+
+    
---
+++ server.py
@@ -0,0 +1,46 @@
+import sqlite3
+import hashlib
+import socket
+import threading
+
+# Connect to SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+
+# Create users table if it doesn't exist (bisa tambah lagi data lain)
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+)
+''')
+conn.commit()
+conn.close()
+
+# Create a socket object
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost', 9999))  #can be set to local IP address if used in a network
+server.listen()
+
+def register(username, password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    try:
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
+        conn.commit()
+        conn.close()
+        return True
+    except sqlite3.IntegrityError:
+        conn.close()
+        print('Username already exists')
+        return False
+
+def login(username, password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, hashed_password))
+    
+    return cursor.fetchone() is not None
